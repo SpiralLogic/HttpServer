d23dfad991784d28393467565187c8d1f4b243f5
Sol Jennings <sol.jennings@myob.com>
2018-04-18 11:56:41 +1000

    Added logger to server

--- HttpServer/Server.cs
+++ HttpServer/Server.cs
@@ -1,17 +1,16 @@
-﻿using System;
-using System.Net;
-using System.Net.Sockets;
-
-namespace HttpServer
+﻿namespace HttpServer
 {
     public class Server 
     {
         private readonly IListener _listener;
+        private ILogger _logger;
         public bool IsRunning => _listener.IsListening;
 
-        public Server(IListener listener)
+        public Server(IListener listener, ILogger logger)
         {
+            _logger = logger;
             _listener = listener;
+            _listener.RequestReceived += (sender, args) => ProcessRequest(args.RequestString);
         }
 
         public void Start()
@@ -24,6 +23,11 @@ namespace HttpServer
             _listener.Stop();
         }
 
+        private void ProcessRequest(string requestString)
+        {
+            _logger.Log(requestString);
+        }
+        
         ~Server()
         {
             _listener.Stop();
--- HttpServerTest/ServerTests.cs
+++ HttpServerTest/ServerTests.cs
@@ -7,28 +7,45 @@ namespace HttpServerTest
 {
     public class ServerTests
     {
+        private readonly TestListener _listener;
+        private readonly TestLogger _logger;
+        private readonly Server _server;
+
+        public ServerTests()
+        {
+            _listener = new TestListener();
+            _logger = new TestLogger();
+            _server = new Server(_listener, _logger);
+        }
+
         [Fact]
         public void HttpServerCanStart()
         {
-            var listener = new TestListener();
-            var server = new Server(listener);
-            server.Start();
-            Assert.True(server.IsRunning);
-            Assert.True(listener.IsListening);
+            _server.Start();
+            Assert.True(_server.IsRunning);
+            Assert.True(_listener.IsListening);
         }
-        
+
         [Fact]
         public void HttpServerCanStop()
         {
-            var listener = new TestListener();
-            var server = new Server(listener);
-            server.Start();
-            server.Stop();
-            Assert.False(server.IsRunning);
-            Assert.False(listener.IsListening);
+            _server.Start();
+            _server.Stop();
+            Assert.False(_server.IsRunning);
+            Assert.False(_listener.IsListening);
+        }
+
+        [Fact]
+        public void HttpServerCanWriteRequestsToLogger()
+        {
+            const string requestString = "Test Request";
+            
+            _server.Start();
+            _listener.SimulateRequest(requestString);
+            
+            Assert.Equal(requestString, _logger.Message);
         }
     }
-
 
     internal class TestListener : IListener
     {
@@ -42,9 +59,24 @@ namespace HttpServerTest
             IsListening = false;
         }
 
+        public void SimulateRequest(string request)
+        {
+            RequestReceived?.Invoke(this, new RequestReceivedEventArgs(request));
+        }
+
         public bool IsListening { get; private set; }
         public Encoding Encoding { get; } = Encoding.ASCII;
         public int Port { get; } = 8000;
         public event EventHandler<RequestReceivedEventArgs> RequestReceived;
+    }
+
+    internal class TestLogger : ILogger
+    {
+        internal string Message { get; private set; } = string.Empty;
+
+        public void Log(string message)
+        {
+            Message = message;
+        }
     }
 }
