14d1abba1a5c5d1d62fb91fee1193614d6bdbc25
Sol Jennings <sol.jennings@myob.com>
2018-05-02 15:57:59 +1000

    * Initial work for basic auth

---
+++ HttpServer/Handlers/BasicAuthHandler.cs
@@ -0,0 +1,20 @@
+﻿using HttpServer.RequestHandlers;
+using HttpServer.Responses;
+using HttpServer.Responses.ResponseCodes;
+
+namespace HttpServer.Handlers
+{
+    public class BasicAuthHandler : IRequestHandler
+    {
+        // TODO: Don't hardcode this
+        private readonly string _user = "admin";
+        private readonly string _password = "password";
+
+        public Response Handle(Request request)
+        {
+            var response = new Response(new Success(), request);
+
+            return response;
+        }
+    }
+}
